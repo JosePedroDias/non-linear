eb63fa0341fe424d22634a4bf304430a09d6316d
José Pedro Dias <jose.pedro.dias@gmail.com>
2019-03-21 15:51:19 +0000

    minors

--- engine.js
+++ engine.js
@@ -10,6 +10,8 @@ fetchAdventure().then((adv) => {
   function doStep(name) {
     const section = adv[name];
     text.innerHTML = section.text;
+    caption.innerHTML = section.caption || '';
+    image.src = 'adventure/' + section.image || '';
   }
 
   doStep('0');

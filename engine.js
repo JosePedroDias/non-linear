c17d75569fe19ec6ea215da9b419f053de2d8cd6
José Pedro Dias <jose.pedro.dias@gmail.com>
2019-03-21 16:02:52 +0000

    first interaction

--- engine.js
+++ engine.js
@@ -10,9 +10,29 @@ fetchAdventure().then((adv) => {
   function doStep(name) {
     const section = adv[name];
     text.innerHTML = section.text;
+    const hasImage = !!section.image;
     caption.innerHTML = section.caption || '';
     image.src = 'adventure/' + section.image || '';
+    image.style.display = hasImage ? '' : 'none';
+    caption.style.display = hasImage ? '' : 'none';
+
+    if (section.interaction) {
+      const inter = section.interaction;
+      if (inter.kind === 'gotos') {
+        interactions.innerHTML = '';
+        inter.options.forEach((opt) => {
+          const el = document.createElement('button');
+          el.appendChild(document.createTextNode(opt.label));
+          el.onclick = function() {
+            doStep(opt.to);
+          };
+          interactions.appendChild(el);
+        });
+      }
+    }
   }
+
+  window.doStep = doStep;
 
   doStep('0');
 });

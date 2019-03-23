43f337a2d8b58f27e46002ad716d60d501ac349f
José Pedro Dias <jose.pedro.dias@gmail.com>
2019-03-23 12:37:43 +0000

    graph generation updated to new syntax

--- generateGraphFromAdventure.js
+++ generateGraphFromAdventure.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const o = JSON.parse(fs.readFileSync('adventure/scenes.json').toString());
 
 function traverse(o, fn) {
-  const t = typeof t;
+  const t = typeof o;
   if (t === 'object') {
     if (o instanceof Array) {
       if (typeof o[0] === 'string') {
@@ -23,18 +23,31 @@ Object.keys(o).forEach((k) => {
 
   const inter = v.interaction;
   if (inter) {
-    const tra = traverse(inter);
-    console.log('k', k);
-    console.log('tra', tra);
+    const actions = { goTo: [] };
+    function fn(arr) {
+      const a = arr[0];
+      const b = arr.slice(1);
+      // console.log(a, b);
+      actions[a].push(b);
+    }
+    traverse(inter, fn);
+    const destinations = actions.goTo.map((arr) => arr[0]);
+    //console.log('k', k);
+    //console.log('actions', actions);
+    //console.log('destinations', destinations);
 
-    out.unshift(`"${k}" [color="blue"]`);
+    destinations.forEach((to) => {
+      out.push(`  "${k}" -> "${to}"`);
+    });
+
+    out.unshift(`  "${k}" [color="blue"]`);
   } else {
     console.log('DEAD END: ' + k);
-    out.unshift(`"${k}" [color="red"]`);
+    out.unshift(`  "${k}" [color="red"]`);
   }
 });
 
-out.unshift('"1" [color="orange"]');
+out.unshift('  "1" [color="orange"]');
 out.unshift('digraph {');
 out.push('}');
 
